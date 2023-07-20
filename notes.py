4c58ee16d872cef3171b1bbc0547fd6f86e261dc
Al-Amin49 <alamin496832@gmail.com>
2023-07-20 21:46:53 +0600

    IPL win probability on the way

---
+++ notes.py
@@ -0,0 +1,8 @@
+#The 𝗵𝗲𝗮𝗱() method returns a specified number of rows, string from the top. The head() method returns the first 
+# 5 rows if a number is not specified.
+''' 
+-matches data set er modde prottekta raw akekta match.
+-match.shape() 2008-19 er modde 756 ta match hoise
+-deliveries dataset a prottek match er prottek ta boler details
+
+'''
